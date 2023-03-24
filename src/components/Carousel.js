aa5991e66aebbc10e1c3cd2c5cd140a5f9b136d5
William Addesso <wsaddesso@gmail.com>
2023-03-24 17:46:46 -0400

    Carousel now returns the band images and informaiton.

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,5 +1,57 @@
+import John2 from '../images/John2.jpg';
+import Mike2 from '../images/Mike2.jpg';
+import Billy2 from '../images/Billy2.jpg';
+import Joe2 from '../images/Joe2.jpg';
+
 const Carousel = () => {
-  return <div>Carousel</div>;
+  const caroArray = [
+    {
+      img: John2,
+      member: 'John Swikata',
+      about:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    },
+    {
+      img: Joe2,
+      member: 'Joe Blau',
+      about:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    },
+    {
+      img: Mike2,
+      member: 'Mike Robbins',
+      about:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    },
+    {
+      img: Billy2,
+      member: 'Billy Addesso',
+      about:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    },
+  ];
+
+  const shuffleArr = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  };
+
+  // Shuffles the array of members so that it is a different order every time.
+  shuffleArr(caroArray);
+
+  let caro = caroArray.map((item) => {
+    return (
+      <div>
+        <img src={item.img}></img>
+        <h2>{item.member}</h2>
+        <p>{item.about}</p>
+      </div>
+    );
+  });
+
+  return <div>{caro}</div>;
 };
 
 export default Carousel;
