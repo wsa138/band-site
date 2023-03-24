a016bd9c03482f7990337df875640c6c6069b9f1
William Addesso <wsaddesso@gmail.com>
2023-03-24 17:27:38 -0400

    Added photos. Started About section. Added new template file Carousel.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,28 @@
+import Band3 from '../images/Band3.jpg';
+import Carousel from '../components/Carousel';
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <div id="band-about">
+        <img src={Band3} alt="Band members"></img>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+      <div id="members-about">
+        <div id="member-carousel">
+          <Carousel />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
