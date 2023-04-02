43080148c699f993fab3f2553515d8aaa4371db7
William Addesso <wsaddesso@gmail.com>
2023-04-02 19:04:25 -0400

    Added album and single artwork.

--- src/pages/Music.js
+++ src/pages/Music.js
@@ -1,18 +1,26 @@
+import ageAlbum from '../images/age-of-the-octopus.jpg';
+import cosmicArt from '../images/cosmic-single-art.webp';
+
 const Music = () => {
   return (
     <div>
       <div>
-        <a href="https://music.amazon.com/albums/B0BZ2VJ6W4" target="blank">
-          Cosmic Outlaw (Single)
+        <a
+          href="https://open.spotify.com/track/4fcIymQJLJkxu4QlnpcvdJ"
+          target="blank"
+        >
+          <img src={cosmicArt} alt="Cosmic Outlaw single artwork"></img>
         </a>
+        <p>Cosmic Outlaw (Single)</p>
       </div>
       <div>
         <a
           href="https://supersonicoctopus.bandcamp.com/album/age-of-the-octopus"
           target="blank"
         >
-          Age of the Octopus (Album)
+          <img src={ageAlbum} alt="Age of the Octopus album artwork"></img>
         </a>
+        <p>Age of the Octopus (Album)</p>
       </div>
     </div>
   );
