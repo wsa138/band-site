357bedb7a01c3ed4f7630564351e4398e48ba33b
William Addesso <wsaddesso@gmail.com>
2023-04-02 23:29:48 -0400

    Added Music.js bootstrap styling.

--- src/pages/Music.js
+++ src/pages/Music.js
@@ -1,27 +1,43 @@
+import { Container, Row, Col, Image } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import ageAlbum from '../images/age-of-the-octopus.jpg';
 import cosmicArt from '../images/cosmic-single-art.webp';
 
 const Music = () => {
   return (
     <div>
-      <div>
-        <a
-          href="https://open.spotify.com/track/4fcIymQJLJkxu4QlnpcvdJ"
-          target="blank"
-        >
-          <img src={cosmicArt} alt="Cosmic Outlaw single artwork"></img>
-        </a>
-        <p>Cosmic Outlaw (Single)</p>
-      </div>
-      <div>
-        <a
-          href="https://supersonicoctopus.bandcamp.com/album/age-of-the-octopus"
-          target="blank"
-        >
-          <img src={ageAlbum} alt="Age of the Octopus album artwork"></img>
-        </a>
-        <p>Age of the Octopus (Album)</p>
-      </div>
+      <Container>
+        <Row className="text-center">
+          <Col md={6} sm={12}>
+            <a
+              href="https://open.spotify.com/track/4fcIymQJLJkxu4QlnpcvdJ"
+              target="blank"
+            >
+              <Image
+                width="300px"
+                thumbnail
+                src={cosmicArt}
+                alt="Cosmic Outlaw single artwork"
+              ></Image>
+            </a>
+            <p>Cosmic Outlaw (Single)</p>
+          </Col>
+          <Col>
+            <a
+              href="https://supersonicoctopus.bandcamp.com/album/age-of-the-octopus"
+              target="blank"
+            >
+              <Image
+                width="300px"
+                thumbnail
+                src={ageAlbum}
+                alt="Age of the Octopus album artwork"
+              ></Image>
+            </a>
+            <p>Age of the Octopus (Album)</p>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
