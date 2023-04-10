1dae8ced4384d8038061be8838f76de933728e2d
William Addesso <wsaddesso@gmail.com>
2023-04-09 22:47:43 -0400

    Made Home.js simply hold a background iamge.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,7 @@
-import banner from '../images/banner.jpg';
-import { Container, Row, Col } from 'react-bootstrap';
 import '../styling/home.css';
 
 const Home = () => {
-  return (
-    <Container id="home-container">
-      <Row id="image-row">
-        <Col xs={12}>
-          <img className="img-fluid" src={banner} alt="Band members"></img>
-        </Col>
-      </Row>
-    </Container>
-  );
+  return <div id="container"></div>;
 };
 
 export default Home;
