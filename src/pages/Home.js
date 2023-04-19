7c670f153813ad3bbea697dc04196a4b46d43620
William Addesso <wsaddesso@gmail.com>
2023-04-18 22:20:28 -0400

    Added video on Home page. Center of width and height.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,19 @@
 import '../styling/home.css';
+import bandVid from '../images/cosmicoutlawvidmp4.mp4';
+import bandVidMov from '../images/cosmicoutlawvid.mov';
+import bandVidWebm from '../images/cosmicoutlawvidwebm.webm';
+import billy from '../images/Billy4.jpg';
 
 const Home = () => {
   return (
     <div id="container">
-      <video autoplay muted loop id="myVideo">
-        <source src="rain.mp4" type="video/mp4" />
-      </video>
+      <video
+        src={bandVidMov}
+        autoplay="true"
+        muted="true"
+        loop="true"
+        id="myVideo"
+      />
     </div>
   );
 };
