f7a39a2f69e627f36ad40bac1e79ccf1ee3c52e4
William Addesso <wsaddesso@gmail.com>
2023-04-18 20:11:39 -0400

    Added video and set up video HTML .

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,13 @@
 import '../styling/home.css';
 
 const Home = () => {
-  return <div id="container"></div>;
+  return (
+    <div id="container">
+      <video autoplay muted loop id="myVideo">
+        <source src="rain.mp4" type="video/mp4" />
+      </video>
+    </div>
+  );
 };
 
 export default Home;
