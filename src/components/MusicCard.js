e856297ab34e15dfb5bdd7cb9131074b793a02d8
William Addesso <wsaddesso@gmail.com>
2025-08-19 22:02:45 -0400

    Added youtube link for Trash

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,48 +1,48 @@
-import ageAlbum from '../images/age-of-the-octopus.jpg';
-import cosmicArt from '../images/CosmicOutlawArt.jpg';
-import kanaloaArt from '../images/KanaloaArt.jpg';
-import polarisArt from '../images/PolarisArt.jpg';
-import trashArt from '../images/trashphoto.png';
-import { Container, Row, Col, Image } from 'react-bootstrap';
+import ageAlbum from "../images/age-of-the-octopus.jpg";
+import cosmicArt from "../images/CosmicOutlawArt.jpg";
+import kanaloaArt from "../images/KanaloaArt.jpg";
+import polarisArt from "../images/PolarisArt.jpg";
+import trashArt from "../images/trashphoto.png";
+import { Container, Row, Col, Image } from "react-bootstrap";
 
 const MusicCard = () => {
   const cardArr = [
     {
-      name: 'Trash In The Hamptons',
-      link: 'https://open.spotify.com/artist/20OK0k4WgurVGcnzKqctmP',
+      name: "Trash In The Hamptons",
+      link: "https://www.youtube.com/watch?v=u4MlP1_Bg-Q",
       art: trashArt,
-      description: 'Trash In The Hamptons single artwork',
+      description: "Trash In The Hamptons single artwork",
     },
     {
-      name: 'Cosmic Outlaw',
-      link: 'https://open.spotify.com/track/4fcIymQJLJkxu4QlnpcvdJ',
+      name: "Cosmic Outlaw",
+      link: "https://open.spotify.com/track/4fcIymQJLJkxu4QlnpcvdJ",
       art: cosmicArt,
-      description: 'Cosmic Outlaw single artwork',
+      description: "Cosmic Outlaw single artwork",
     },
     {
-      name: 'Kanaloa',
-      link: 'https://open.spotify.com/track/3kfDz05GkqYKSMhYFnwXbr',
+      name: "Kanaloa",
+      link: "https://open.spotify.com/track/3kfDz05GkqYKSMhYFnwXbr",
       art: kanaloaArt,
-      description: 'Kanaloa single artwork',
+      description: "Kanaloa single artwork",
     },
     {
-      name: 'Polaris',
-      link: 'https://open.spotify.com/track/0NGXp3WXrCxyB1qb5G9HPU',
+      name: "Polaris",
+      link: "https://open.spotify.com/track/0NGXp3WXrCxyB1qb5G9HPU",
       art: polarisArt,
-      description: 'Polaris single artwork',
+      description: "Polaris single artwork",
     },
     {
-      name: 'Age of the Octopus (Album)',
-      link: 'https://supersonicoctopus.bandcamp.com/album/age-of-the-octopus',
+      name: "Age of the Octopus (Album)",
+      link: "https://supersonicoctopus.bandcamp.com/album/age-of-the-octopus",
       art: ageAlbum,
-      description: 'Age of the octopus album artwork',
+      description: "Age of the octopus album artwork",
     },
   ];
 
   const handleDataLayer = (clickName) => {
     window.dataLayer = window.dataLayer || [];
     window.dataLayer.push({
-      event: 'musicClick',
+      event: "musicClick",
       clickText: clickName,
     });
   };
